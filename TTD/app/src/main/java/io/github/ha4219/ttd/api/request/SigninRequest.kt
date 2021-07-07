35d801e15538c4e47db0e4ea5ac8e69ddd2747a7
정동하 <ha4219@naver.com>
2021-07-07 15:53:18 +0900

    [ha4219/android] create SigninRequest class

---
+++ TTD/app/src/main/java/io/github/ha4219/ttd/api/request/SigninRequest.kt
@@ -0,0 +1,16 @@
+package io.github.ha4219.ttd.api.request
+
+import android.util.Patterns
+
+class SigninRequest (
+    val email: String?,
+    val password: String?
+) {
+    fun isNotValidEmail() =
+        email.isNullOrBlank()
+                || !Patterns.EMAIL_ADDRESS.matcher(email).matches()
+
+    fun isNotValidPassword() =
+        password.isNullOrBlank()
+                || password.length !in 8..20
+}
