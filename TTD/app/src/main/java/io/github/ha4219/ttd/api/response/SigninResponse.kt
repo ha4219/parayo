2221acbeefed815bfadc7127c28d2cb6ed7aa679
정동하 <ha4219@naver.com>
2021-07-07 16:31:19 +0900

    [ha4219/android] add func onSigninResponse in SignupViewModel

--- TTD/app/src/main/java/io/github/ha4219/ttd/api/response/SigninResponse.kt
+++ TTD/app/src/main/java/io/github/ha4219/ttd/api/response/SigninResponse.kt
@@ -1,8 +1,11 @@
 package io.github.ha4219.ttd.api.response
 
+import io.github.ha4219.ttd.common.Prefs
+
 
 data class SigninResponse(
     val token: String,
+    val refreshToken: String,
     val userName: String,
     val userId: Long
 )
--- TTD/app/src/main/java/io/github/ha4219/ttd/signup/SignupViewModel.kt
+++ TTD/app/src/main/java/io/github/ha4219/ttd/signup/SignupViewModel.kt
@@ -5,6 +5,8 @@ import androidx.lifecycle.MutableLiveData
 import io.github.ha4219.ttd.api.DongApi
 import io.github.ha4219.ttd.api.request.SignupRequest
 import io.github.ha4219.ttd.api.response.ApiResponse
+import io.github.ha4219.ttd.api.response.SigninResponse
+import io.github.ha4219.ttd.common.Prefs
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.withContext
 import net.codephobia.ankomvvm.lifecycle.BaseViewModel
@@ -58,6 +60,19 @@ class SignupViewModel(app: Application):BaseViewModel(app) {
         if(response.success){
             toast("회원 가입이 완료되었습니다. 로그인 후 이용해주세요.")
             finishActivity()
+        }else{
+            toast(response.message ?: "알 수 없는 오류가 발생했습니다.")
+        }
+    }
+
+    private fun onSigninResponse(response: ApiResponse<SigninResponse>) {
+        if(response.success && response.data!= null){
+            Prefs.token = response.data.token
+            Prefs.refreshToken = response.data.refreshToken
+            Prefs.userName = response.data.userName
+            Prefs.userId = response.data.userId
+
+            toast("로그인되었습니다.")
         }else{
             toast(response.message ?: "알 수 없는 오류가 발생했습니다.")
         }
