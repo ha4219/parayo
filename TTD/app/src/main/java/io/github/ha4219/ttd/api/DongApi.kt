2be34056fcd055f3b70cd264cd83db41d48005e7
정동하 <ha4219@naver.com>
2021-07-07 15:57:06 +0900

    [ha4219/android] add func signin post method

--- TTD/app/src/main/java/io/github/ha4219/ttd/api/DongApi.kt
+++ TTD/app/src/main/java/io/github/ha4219/ttd/api/DongApi.kt
@@ -1,8 +1,10 @@
 package io.github.ha4219.ttd.api
 
 
+import io.github.ha4219.ttd.api.request.SigninRequest
 import io.github.ha4219.ttd.api.request.SignupRequest
 import io.github.ha4219.ttd.api.response.ApiResponse
+import io.github.ha4219.ttd.api.response.SigninResponse
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
@@ -20,4 +22,8 @@ interface DongApi{
     @POST("/api/v1/users")
     suspend fun signup(@Body signupRequest: SignupRequest)
         :ApiResponse<Void>
+
+    @POST("/api/v1/signin")
+    suspend fun signin(@Body signinRequest: SigninRequest)
+        :ApiResponse<SigninResponse>
 }
