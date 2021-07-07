9bae4ac1a41a7f0037b16ccdd714afed03bffbe6
정동하 <ha4219@naver.com>
2021-07-07 15:50:58 +0900

    [ha4219/android] create SigninActivity class

---
+++ TTD/app/src/main/java/io/github/ha4219/ttd/signin/SigninActivity.kt
@@ -0,0 +1,9 @@
+package io.github.ha4219.ttd.signin
+
+
+import net.codephobia.ankomvvm.components.BaseActivity
+
+
+class SigninActivity : BaseActivity<SigninViewModel>() {
+    override val viewModelType = SigninViewModel::class
+}
