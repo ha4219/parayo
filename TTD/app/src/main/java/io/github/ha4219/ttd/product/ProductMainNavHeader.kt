ecd972117a71800f2b4495d95a5a35e1d98ccc12
정동하 <ha4219@naver.com>
2021-07-08 22:35:28 +0900

    [ha4219/android] add drawbleMenu items

--- TTD/app/src/main/java/io/github/ha4219/ttd/product/ProductMainNavHeader.kt
+++ TTD/app/src/main/java/io/github/ha4219/ttd/product/ProductMainNavHeader.kt
@@ -1,4 +1,27 @@
 package io.github.ha4219.ttd.product
 
-class ProductMainNavHeader {
+import android.graphics.Typeface
+import android.view.View
+import io.github.ha4219.ttd.R
+import io.github.ha4219.ttd.common.Prefs
+import io.github.ha4219.ttd.view.borderBottom
+import org.jetbrains.anko.*
+
+
+class ProductMainNavHeader : AnkoComponent<View> {
+    override fun createView(ui: AnkoContext<View>) =
+        ui.verticalLayout {
+            padding = dip(20)
+            background = borderBottom(width = dip(1))
+
+            imageView(R.drawable.ic_logo)
+                .lparams(dip(54), dip(54))
+
+            textView(Prefs.userName) {
+                topPadding = dip(8)
+                textSize = 20f
+                typeface = Typeface.DEFAULT_BOLD
+            }
+        }
+
 }
--- TTD/app/src/main/java/io/github/ha4219/ttd/product/ProductMainUI.kt
+++ TTD/app/src/main/java/io/github/ha4219/ttd/product/ProductMainUI.kt
@@ -1,12 +1,16 @@
 package io.github.ha4219.ttd.product
 
 import android.view.Gravity
+import android.view.Menu.NONE
+import android.view.MenuItem
 import android.view.MenuItem.SHOW_AS_ACTION_ALWAYS
 import androidx.appcompat.widget.Toolbar
 import org.jetbrains.anko.appcompat.v7.toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 import io.github.ha4219.ttd.R
+import io.github.ha4219.ttd.common.Prefs
+import io.github.ha4219.ttd.signin.SigninActivity
 import io.github.ha4219.ttd.view.borderBottom
 import org.jetbrains.anko.*
 import org.jetbrains.anko.design.navigationView
@@ -15,7 +19,8 @@ import org.jetbrains.anko.support.v4.drawerLayout
 
 class ProductMainUI(
     private val viewModel: ProductMainViewModel
-) : AnkoComponent<ProductMainActivity> {
+) : AnkoComponent<ProductMainActivity>,
+    NavigationView.OnNavigationItemSelectedListener{
 
     lateinit var navigationView: NavigationView
     lateinit var toolBar: Toolbar
@@ -37,8 +42,42 @@ class ProductMainUI(
 
 
             navigationView = navigationView {
+                ProductMainNavHeader()
+                    .createView(AnkoContext.create(context, this))
+                    .run(::addHeaderView)
+
+                menu.apply {
+                    add(NONE,MENU_ID_INQUIRY,NONE, "내 문의").apply {
+                        setIcon(R.drawable.ic_chat)
+                    }
+                    add(NONE,MENU_ID_LOGOUT,NONE, "로그아웃").apply {
+                        setIcon(R.drawable.ic_logout)
+                    }
+
+                    setNavigationItemSelectedListener(this@ProductMainUI)
+                }
             }.lparams(wrapContent, matchParent) {
                 gravity = Gravity.START
             }
         }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            MENU_ID_INQUIRY->{viewModel.toast("내 문의")}
+            MENU_ID_LOGOUT->{
+                Prefs.token = null
+                Prefs.refreshToken = null
+                viewModel.startActivityAndFinish<SigninActivity>()
+            }
+        }
+
+        drawerLayout.closeDrawer(navigationView)
+
+        return true
+    }
+
+    companion object{
+        private const val MENU_ID_INQUIRY = 1
+        private const val MENU_ID_LOGOUT = 2
+    }
 }
